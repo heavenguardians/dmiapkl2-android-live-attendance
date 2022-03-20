b289334b67e8c2f34e7da2d797d23effd580b452
Your Name <you@example.com>
2022-03-20 22:55:17 +0700

    ui: prepare MainActivity for display

--- app/src/main/java/com/example/dmiapklliveattendanceapp/views/forgotpass/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/dmiapklliveattendanceapp/views/forgotpass/ForgotPasswordActivity.kt
@@ -1,4 +1,4 @@
-package com.example.liveattendanceapp.views.forgotpass
+package com.example.dmiapklliveattendanceapp.views.forgotpass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
